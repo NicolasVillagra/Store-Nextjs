f2708cc1b318965e540a44c83f3f55b0638b7ed5
JoseNicolas <villagrajosenicolas@gmail.com>
2023-10-03 19:25:40 -0300

    update

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from '@nextui-org/button'; 
+
+const page = () => {
+  return (
+    <div>
+    <h1>HOLA MUNDO</h1>
+    <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default page
